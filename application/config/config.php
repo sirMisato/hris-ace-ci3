656ce726f3f4be5d823bcf23e2efc6bce6f7af8e
ridhaakmal <ridhaakmalputra@gmail.com>
2022-04-20 16:19:55 +0700

    ganti config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/new/';
+$config['base_url'] = 'http://localhost/hris-ace-ci3/';
 
 /*
 |--------------------------------------------------------------------------
